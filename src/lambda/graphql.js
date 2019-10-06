8974fb292be40c104c01b9d9223a767ab292b8da
Gaurav Chikhale <gauravchl@users.noreply.github.com>
2019-10-06 13:35:54 -0400

    Add CORS headers for netlify function, and a script to run funtions locally

--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -5,5 +5,9 @@ exports.handler = async (request, context) => {
   await setup.run();
   const response = await handleRequest(request, context);
 
+  response.headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+  }
   return response;
 };
